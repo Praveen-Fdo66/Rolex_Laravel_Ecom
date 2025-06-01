c03f8f0798f1732ad55b952a564c791872dd1bcc
Praveen Fernando <fernandojr662005@gmail.com>
2025-06-01 23:11:03 +0530

    gcs adding but failed

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,18 +47,18 @@ return [
             'report' => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
-        ],
+
+        'gcs' => [
+             'driver' => 'gcs',
+             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
+             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
+             'key_file' => env('GOOGLE_CLOUD_KEY_FILE'), // Absolute path to JSON key
+             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null),
+             'apiUri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null),
+             'visibility' => 'public',
+],
+
+
 
     ],
 
